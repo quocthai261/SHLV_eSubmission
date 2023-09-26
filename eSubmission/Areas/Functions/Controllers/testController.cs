f12c279e178344dcea8881c75fca6782f70d7623
pdkladykillah <khoapham3900@gmail.com>
2023-09-26 15:17:41 +0700

    Test

---
+++ eSubmission/Areas/Functions/Controllers/testController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eSubmission.Areas.Functions.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
